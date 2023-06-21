0ce83749b99ff6923e32b28ea8b3a0e401529e33
Ken-Andre <andre.kenmogne@2026.ucac-icam.com>
2023-06-21 21:11:48 +0100

    initialise the project part using create-react-app

---
+++ src/pages/Login.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import CustomInput from "../components/CustomInput";
+import { Link, useNavigate } from "react-router-dom";
+// import * as yup from "yup";
+// import { useFormik } from "formik";
+// import { useDispatch, useSelector } from "react-redux";
+// import { login } from "../features/auth/authSlice";
+
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login;
